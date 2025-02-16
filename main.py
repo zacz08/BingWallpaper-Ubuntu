ba1a3276f38fb67c88ce310c694ee7320cd1386b
zacz08 <zacharyzhang95@163.com>
2025-02-16 19:00:45 +0000

    update image url getting method (test on Ubuntu 24.04)

--- main.py
+++ main.py
@@ -3,11 +3,28 @@ import sys
 import os
 import requests
 import hashlib
-url = "http://bingw.jasonzeng.dev?resolution=UHD&index="
 
 def getImg(url):
     response = requests.get(url)
     return response.content
+
+def get_bing_wallpaper(index='0', resolution="UHD"):
+    # Accessing the Bing Wallpaper API
+    api_url = f"https://www.bing.com/HPImageArchive.aspx?format=js&idx={index}&n=1&mkt=zh-CN"
+    response = requests.get(api_url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        if "images" in data and len(data["images"]) > 0:
+            raw_url = data["images"][0]["url"]
+            url_without_params = raw_url.split('&')[0]  # Remove useless parameters
+            url_base = url_without_params.rsplit('_', 1)[0] # Remove the resolution part
+            high_res_url = f"https://www.bing.com{url_base}_{resolution}.jpg"
+            response = requests.get(high_res_url)
+            return response.content
+    notify = 'Can not get the image from Bing Wallpaper API'
+    os.system(notify)
+    return None
 
 # Check whether the image obtained from the api is already exists locally
 def compare(data):
@@ -34,9 +51,7 @@ if __name__ == "__main__":
         picturePath = sys.argv[1] + '/picture/background.jpg'
         
         # splice a complete URL with argv[2] (index, number or ramdom)
-        img_url = url + sys.argv[2]
-
-        img_data = getImg(img_url)
+        img_data = get_bing_wallpaper(index=sys.argv[2])
         if not compare(img_data):
             with open(picturePath, 'wb') as img:
                 img.write(img_data)
